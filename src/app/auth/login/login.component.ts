03decfa6f47dd42c21379b857381f14581eca4c7
minsur <criizt.mart@gmail.com>
2022-10-27 11:44:19 -0500

    validacion de usuario

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { AuthService } from '../service/auth.service';
 
@@ -13,7 +14,8 @@ export class LoginComponent implements OnInit {
 
   constructor( 
     private router : Router, 
-    private authService : AuthService
+    private authService : AuthService,
+    private matSnackBar: MatSnackBar
   ) { 
     this.builform();
   }
@@ -33,6 +35,13 @@ export class LoginComponent implements OnInit {
     const userLogin = this.Form.getRawValue();
     this.authService.signIn(userLogin).subscribe((resp) => {
       this.onList();  
+    }, error => {
+      this.matSnackBar.open(
+        'El usuario no existe, intente con el usuario cristian@gmail.com y la contraseña 12345678',
+        'Cerrar',
+        { duration: 5000, verticalPosition: 'top', horizontalPosition: 'end' }
+      );
+       return;
     });
   }
 
